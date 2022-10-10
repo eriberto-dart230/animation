e0ba423df723fc58bb04380b85db9c7256c3dc11
eriberto-dart230 <113368656+eriberto-dart230@users.noreply.github.com>
2022-10-09 21:26:30 -0400

    Add files via upload

---
+++ Animation/sketch.js
@@ -0,0 +1,108 @@
+function setup() {
+  createCanvas(700, 350);
+}
+
+function draw() {
+  
+ let size=100;
+  
+  
+  background(181, 97, 29);
+
+  fill(119,221);
+  rect(0,250,1300,250)
+ 
+  
+  strokeWeight(4);
+  stroke(51);
+  fill(230,207,12);
+  rect(240,100,10,100);
+  
+  
+  fill(230,207,12);
+  rect(130,100,10,100);
+  //line(30,30,350,100);
+  
+  stroke(50);
+  fill(255);
+  rect(140,100,100,160);
+  
+  strokeWeight(3);
+  ellipse(190,59,size);
+  
+  
+  rect(140,200,50,100);
+  rect(190,200,50,100);
+  
+  ellipse(169,59,10);
+  ellipse(210,59,10);
+  square(400, 20, 270, 20);
+  fill(45);
+  triangle(145, 75, 188, 110, 238, 75);
+  
+  fill(67);
+  
+  triangle(185, 0, 140, 25, 238, 25);
+  
+  
+  let c = color(255, 255, 255);
+  fill(c);
+  triangle(150, 75, 165, 90, 168, 75);
+  triangle(170, 75, 178, 90, 190, 75);
+  triangle(190, 75, 200, 90, 210, 75);
+  triangle(210, 75, 210, 90, 230, 75);
+  
+  triangle(170, 95, 180, 90, 180, 105);
+  //triangle(189, 108, 190, 90, 180, 105);
+  triangle(210, 95, 200, 90, 200, 105);
+   triangle(200, 107, 190, 90, 180, 105);
+  
+  line(450,50,450,136);
+  line(425,51,475,51);
+  line(490,96,490,136);
+  line(490,96,510,96);
+  line(520,96,520,136);
+  line(520,85,520,91);
+  arc(560, 113, 50, 50, 0, PI + QUARTER_PI, PIE);
+  line(600,56,600,136);
+  line(630,70,600,100);
+  line(600,100,630,135);
+  
+  fill(255,140,0);
+  ellipse(540,220,90);
+  fill(0,0,0);
+  ellipse(540,200,45, 30);
+  
+  
+  fill(67);
+  ellipse(450,350,350,124);
+  fill(255,140,0);
+  ellipse(450,300,80);
+  fill(0,0,0);
+  ellipse(450,280,45, 30);
+
+  
+  
+  fill(c);
+  let xPos = 0;
+  let yPos= 0;
+ellipse(xPos,yPos,200,200);
+
+
+  
+  //ellipse();
+  //line();
+ // line();
+  
+ 
+  //circle(xPos, yPos, 70);
+  //circle(50,50,30);
+ // circle(80,80,50);
+  
+
+
+  
+  
+  
+  
+}
